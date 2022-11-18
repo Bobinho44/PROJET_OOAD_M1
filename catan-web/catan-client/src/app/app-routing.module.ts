553ec8f65cbbf61d438e7455be12a37d5c169659
Marius GUITTON-FRANTZ <marius.guitton-frantz@etu.univ-nantes.fr>
2022-11-18 16:13:28 +0100

    adding routing module

--- catan-web/catan-client/src/app/app-routing.module.ts
+++ catan-web/catan-client/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoardComponent } from './components/board/board.component';
+import { LobbyComponent } from './components/lobby/lobby.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LobbyComponent },
+  { path: 'board', component: BoardComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- catan-web/catan-client/src/app/app.module.ts
+++ catan-web/catan-client/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LobbyComponent } from './components/lobby/lobby.component';
-import { BoardComponent } from './board/board.component';
+import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
   declarations: [
