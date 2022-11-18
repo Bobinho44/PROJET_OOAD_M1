c752bcb2d6b377911b354ede74c4366fae657bce
Marius GUITTON-FRANTZ <marius.guitton-frantz@etu.univ-nantes.fr>
2022-11-18 14:31:38 +0100

    generate board component

--- catan-web/catan-client/src/app/app.module.ts
+++ catan-web/catan-client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LobbyComponent } from './components/lobby/lobby.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LobbyComponent
+    LobbyComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
